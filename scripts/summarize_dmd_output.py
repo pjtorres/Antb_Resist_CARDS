3194b85b32500dfb092fb98c6487d578943a0372
Pedro J. Torres <pjtorres88@gmail.com>
2018-02-11 15:25:01 -0800

    Update summarize_dmd_output.py

--- scripts/summarize_dmd_output.py
+++ scripts/summarize_dmd_output.py
@@ -5,18 +5,16 @@ import os
 import pandas
 import pandas as pd
 
-"Summarize diamond output (6 stitle format). Output is two files: one file is raw counts, one is relative abundance. "
+"Summarize Diamond output (6 stitle format). Output is two files: one file is raw counts, one is relative abundance."
 
 #---------- Split to count the number of hits given by a blast output and make an OTU style format------
 parser=argparse.ArgumentParser(description="Script will sum up your blast text output into an otu style format. For this script to work output you must output your blastn data like this '-outfmt 6 stitle'. Also make sure all your blast outputs you want to summarize are in their own directory")
 parser.add_argument('-d','--dir', help='All blast.txt files should be in one directory. Add directory here or leave blank for current working firectory', required=False)
 parser.add_argument('-o','--out', help='Name of summary ouput text file', required=True)#require later
-parser.add_argument('-o_ra','--outra', help='Name of summary ouput text file with relative gene abundance', required=True)#require later
 
 args=parser.parse_args()
 directory_i=str(args.dir)
 o_file=str(args.out)
-relative_abun=str(args.outra)
 
 #----- check to see if you input you input the directory where files are kept--------
 if args.dir is None:
@@ -28,7 +26,7 @@ else:
     directory=cwd+directory_i
 
 #-----------------Make dictionary and call each txt file in directory-------------------------------
-"""make dictionary to keep count of DIAMOND outputs. Keys are going to be taxa name and a list will keep track of the abundance for each file"""
+"""make dictionary to keep count of DIAMOND outputs. Keys are going to be gene/taxa name and a list will keep track of the abundance for each file"""
 taxa={}
 # All txt files (should be blast output txt files) in current directory are added to a list 'F'
 F=[directory+i for i in os.listdir(directory) if i.split(".")[-1]=="txt"]
@@ -40,16 +38,29 @@ for txt in  F:
     f=open(txt,"r")
     for line in f:
         line=line.strip()
-        line=line.split('   ')# split based on this character and add them individually to a list
-        line=line[1:]
-        line=" ".join(line)
-        line=line.split()
-        taxName=" ".join(line[0:])# get name of Taxa this can be changed if you want GI numbers or something else isntead
-        """Add taxa/gene as key in dictionary and its value is a list in which each element represents taxa/gene count for each file.Checks to see if Taxa is already in the dictionary, if not, it will add it and and start to makes a list with a zero to keep count each time it sees that taxa it will add 1."""
-        if taxName not in taxa:
-            taxa[taxName]=[0]*len(F)# Make a list for to keep the Taxa abundace for each file in directory or length of F.
-        else:pass
-        taxa[taxName][F.index(txt)]+=1
+        if dbtype == 'vf':
+            line=line.split('   ')# split based on this character and add them individually to a list
+            line=line[1:]
+            line=" ".join(line)
+            line=line.split()
+            taxName=" ".join(line[0:])# get name of Taxa this can be changed if you want GI numbers or something else isntead
+            """Add taxa/gene as key in dictionary and its value is a list in which each element represents taxa/gene count for each file.Checks to see if Taxa is already in the dictionary, if not, it will add it and and start to makes a list with a zero to keep count each time it sees that taxa it will add 1."""
+            if taxName not in taxa:
+                taxa[taxName]=[0]*len(F)# Make a list for to keep the Taxa abundace for each file in directory or length of F.
+            else:pass
+            taxa[taxName][F.index(txt)]+=1
+        else:
+            line=line.split(' ')# split based on this character and add them individually to a list for atb. Space is different in seperator
+            line=line[1:]
+            line=" ".join(line)
+            line=line.split()
+            taxName=" ".join(line[0:])# get name of Taxa this can be changed if you want GI numbers or something else isntead
+            """Add taxa/gene as key in dictionary and its value is a list in which each element represents taxa/gene count for each file.Checks to see if Taxa is already in the dictionary, if not, it will add it and and start to makes a list with a zero to keep count each time it sees that taxa it will add 1."""
+            if taxName not in taxa:
+                taxa[taxName]=[0]*len(F)# Make a list for to keep the Taxa abundace for each file in directory or length of F.
+            else:pass
+            taxa[taxName][F.index(txt)]+=1
+            
 print ("There are a total of " +str(len(taxa)) +"  genes")
 
 #-----------------Create new output file and write in abundance
@@ -90,6 +101,6 @@ dft=df['Gene Bacteria'].str.split('[',1, expand=True).rename(columns={0:'Gene', 
 df= df.drop(['Gene Bacteria'], axis=1)
 final =pd.concat([dft,df], axis=1)
 os.remove("tmp2.csv")
-final.to_csv(relative_abun+'.csv',index=False)
+final.to_csv(o_file+'_relativeabundance'+'.csv',index=False)
 
 print ('DONE :)')
